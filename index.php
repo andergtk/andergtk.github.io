2901fef76c0b93ccf1da01c865f0681c060c4912
Anderson Camargo <andergtk@gmail.com>
2016-05-05 03:44:37 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Anderson Camargo</title>
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600">
+		<style>
+			html,
+			body {
+				width: 100%;
+				height: 100%;
+			}
+
+			body {
+				margin: 0;
+				min-height: 100%;
+				background-color: #1C2A39;
+			}
+
+			h1 {
+				position: absolute;
+				margin-top: -0.8em;
+				margin-left: -4.2em;
+				top: 50%;
+				left: 50%;
+				color: #fff;
+				font-family: "Source Sans Pro";
+				font-size: 3.1em;
+				font-weight: 600;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Anderson Camargo</h1>
+	</body>
+</html>
